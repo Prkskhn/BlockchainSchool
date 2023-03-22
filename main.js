4ad0746251898fc79a080b76903ba01fa1a45734
Prkskhn <tjrgns7452@gmail.com>
2023-03-22 17:34:56 +0900

    get date

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const time = document.querySelector(".time");
 
+const newDate = new Date();
+
+const hours = newDate.getHours();
+
+const minutes = newDate.getMinutes();
+
+const seconds = newDate.getSeconds();
+
+time.innerText = hours + ":" + minutes + ":" + seconds;
+
+console.log(time);
