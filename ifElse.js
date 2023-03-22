d321565241f16857296f42b0504ff6508dcf0b3f
Prkskhn <tjrgns7452@gmail.com>
2023-03-22 15:17:46 +0900

    ifElse

---
+++ ifElse.js
@@ -0,0 +1,37 @@
+// const BLOCK = "블";
+// const BBLE = "쁠";
+// const CHAIN = "체";
+// const SCHOOL = "스";
+
+// let rightWord = BLOCK + CHAIN + SCHOOL;
+
+// if (rightWord === "블체스") {
+//   console.log("블체스 화이팅");
+// }
+
+// const 나는프로그래머다 = true;
+// const 나는천개의비트코인이있다 = false;
+// const 내가제일좋아하는음료는제로콜라다 = true;
+
+// let trueOrFalse = 나는천개의비트코인이있다;
+
+// if (trueOrFalse) {
+//   console.log("당신은 거짓을 말하지 않는 선량한 가람");
+// } else {
+//   console.log("라이어새끼야");
+// }
+
+let blockchainScore = 13;
+let frontendScore = 47;
+
+let totalScore = blockchainScore + frontendScore;
+
+if (totalScore > 78) {
+  console.log("Great");
+} else if (totalScore > 50) {
+  console.log("good");
+} else if (totalScore > 30) {
+  console.log("try harder");
+} else {
+  console.log("fuck");
+}
