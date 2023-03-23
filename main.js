2f738d7699e23c495b7ac84a7d224e4076d7d2f5
Prkskhn <tjrgns7452@gmail.com>
2023-03-23 19:59:00 +0900

    setClock

---
+++ main.js
@@ -0,0 +1,22 @@
+function timeChecker() {
+  let time = document.querySelector(".time");
+
+  let newDate = new Date();
+  let hours = newDate.getHours();
+  let minutes = newDate.getMinutes();
+  let seconds = newDate.getSeconds();
+
+  if (hours.toString().length < 2) {
+    hours = "0" + hours.toString();
+  }
+  if (minutes.toString().length < 2) {
+    minutes = "0" + minutes.toString();
+  }
+  if (seconds.toString().length < 2) {
+    seconds = "0" + seconds.toString();
+  }
+
+  time.innerText = `${hours}:${minutes}:${seconds}`;
+}
+timeChecker();
+setInterval(timeChecker, 1000);
