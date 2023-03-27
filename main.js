23aa61d035faa3c0a7cde422031f0a366ca9f9e6
Prkskhn <tjrgns7452@gmail.com>
2023-03-27 17:36:52 +0900

    quotes random

---
+++ main.js
@@ -0,0 +1,26 @@
+function getTime() {
+  const newDate = new Date();
+
+  const hours = newDate.getHours().toString().padStart(2, "0");
+  const minutes = newDate.getMinutes().toString().padStart(2, "0");
+  const seconds = newDate.getSeconds().toString().padStart(2, "0");
+
+  let timeView = document.querySelector(".time");
+
+  timeView.innerText = `${hours}:${minutes}:${seconds}`;
+}
+getTime();
+setInterval(getTime, 1000);
+
+let quotesInput = document.querySelector(".quotes");
+let getQuotes = localStorage.getItem("quotes");
+
+if (!getQuotes) {
+  localStorage.setItem("quotes", JSON.stringify(["명언1", "명언2", "명언3"]));
+  getQuotes = localStorage.getItem("quotes");
+}
+
+let displayQuotes = JSON.parse(getQuotes);
+
+quotesInput.innerText =
+  displayQuotes[Math.floor(Math.random() * displayQuotes.length)];
