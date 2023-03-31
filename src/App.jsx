27f3992783a97907eb47f9a7fc0364cbc12ebce1
Prkskhn <tjrgns7452@gmail.com>
2023-03-31 15:47:47 +0900

    react usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">11</div>;
+  const [count, setCount] = useState(10);
+
+  const onClickAdd = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <div className="bg-red-100">{count}</div>
+      <button className="bg-green-300" onClick={onClickAdd}>
+        +
+      </button>
+    </>
+  );
 }
 
 export default App;
