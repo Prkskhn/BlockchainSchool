5d94cf8f45d293ed0ced42253df50e19ef2e3761
Prkskhn <tjrgns7452@gmail.com>
2023-03-31 18:03:48 +0900

    class edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">11</div>;
+  const [count, setCount] = useState(11);
+  const onClickAdd = () => {
+    setCount(count);
+  };
+
+  return (
+    <>
+      <div className="App bg-red-300">{count}</div>
+      <button className="bg-emerald-300" onClick={onClickAdd}>
+        +
+      </button>
+    </>
+  );
 }
 
 export default App;
