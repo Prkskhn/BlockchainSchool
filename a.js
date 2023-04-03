c0c13945ca80fccea69004a6177f14912cab3509
Prkskhn <tjrgns7452@gmail.com>
2023-04-03 14:35:25 +0900

    ternary operator

---
+++ a.js
@@ -0,0 +1,26 @@
+// const fruits = ["apple", "melon", "orange"];
+
+// console.log(fruits.length);
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// fruits.map((v, i) => {
+//   console.log(`${i + 1}번째 과일: ${v}`);
+// });
+
+const array = [1, 2, 4, 8];
+console.log(array);
+
+const squareArray = array.map((v) => {
+  return v * v;
+});
+
+const squareArray2 = [];
+for (let i = 0; i < array.length; i++) {
+  squareArray2.push(array[i] * array[i]);
+}
+
+console.log(squareArray);
+console.log(squareArray2);
--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,12 +1,23 @@
-function Date() {
+function Date(props) {
+  console.log(props);
+
   return (
     <li className="inline-block bg-white rounded-lg shadow-md ">
-      <div className="bg-red-400 rounded-t-lg px-2 py-1 text-xl text-white">
-        2
+      <div
+        className={`${
+          props.day === "SUN"
+            ? "bg-red-400"
+            : props.day === "SAT"
+            ? "bg-orange-400"
+            : "bg-blue-400"
+        } rounded-t-lg px-2 py-1 text-xl text-white`}
+      >
+        {props.date}
       </div>
-      <ul className="p-2 text-lg">
-        <li>청소하기</li>
-        <li>빨래하기</li>
+      <ul className="p-2 text-lg h-20">
+        {props.todos.map((v, i) => {
+          return <li>{v}</li>;
+        })}
       </ul>
     </li>
   );
