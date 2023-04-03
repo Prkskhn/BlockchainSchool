754b58854b99b68ecd591b3611266095ba5102e7
Prkskhn <tjrgns7452@gmail.com>
2023-04-03 11:23:35 +0900

    destructurize

---
+++ a.js
@@ -0,0 +1,22 @@
+// const myProfile = {
+//   nickName: "yalralloo",
+//   age: 18,
+//   job: "nothing",
+//   isGenius: false,
+// };
+
+// const { nickName } = myProfile;
+// console.log(nickName);
+// console.log(myProfile.nickName);
+
+const yalralloo = {
+  nickName: "yalralloo",
+};
+
+const yariyalloo = {
+  nickName: "yariyalloo",
+};
+
+const { nickName } = yalralloo;
+const { nickName: newNick } = yariyalloo;
+console.log(newNick);
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import { fakeUseState } from "../fakeHooks";
+
 function Card(props) {
+  const [count, setCount] = useState();
+  const [num1, setnum1] = useState();
+  const [num2, setnum2] = useState();
+
+  const { value, setValue } = fakeUseState();
+  const { value: num3, setValue: setNum3 } = fakeUseState();
+  const { value: num4, setValue: setNum4 } = fakeUseState();
+
   return <div className={`${props.color} w-40 h-60`}>{props.cardName}</div>;
 }
 
