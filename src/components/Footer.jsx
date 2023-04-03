0f80cc5003ac3d8d4bc56e1664f25e0aeca25bf8
Prkskhn <tjrgns7452@gmail.com>
2023-04-03 19:57:32 +0900

    router

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="h-20 min-w-full bg-purple-300 flex justify-center items-center  ">
+      Footer
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/pages/a.jsx
@@ -0,0 +1,9 @@
+function A() {
+  return (
+    <div className="min-w-full bg-blue-300 flex justify-center items-center grow ">
+      A
+    </div>
+  );
+}
+
+export default A;
