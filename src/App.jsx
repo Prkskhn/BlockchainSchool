7f353d3ec4c8c0855079c49f84b78aca3f56a340
Prkskhn <tjrgns7452@gmail.com>
2023-04-03 17:41:47 +0900

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Card from "./components/Card";
+import cardData from "./cardData.json";
+
 function App() {
-  return <div className="App">11</div>;
+  return (
+    <div className="grid grid-cols-4 justify-items-center gap-8 py-8">
+      {cardData.map((v, i) => {
+        return <Card key={i} symbol={v.symbol} score={v.score} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
