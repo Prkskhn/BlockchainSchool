156ff9fc76b4626a1f663e1d7a12d1fb5691c88a
Prkskhn <tjrgns7452@gmail.com>
2023-04-04 15:20:10 +0900

    image path

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,15 +1,18 @@
 import PortfolioCard from "./PortfolioCard";
-
+import projectData from "../projectData.json";
 const Portfolio = () => {
   return (
     <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
+      {projectData.map((v, i) => {
+        return (
+          <PortfolioCard
+            key={i}
+            title={v.title}
+            desc={v.desc}
+            image={v.image}
+          />
+        );
+      })}
     </ul>
   );
 };
--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,12 +1,16 @@
-const PortfolioCard = () => {
+const PortfolioCard = ({ title, desc, image }) => {
   return (
     <li className="bg-purple-300 w-60 h-72 rounded-xl">
       {/* object-fit:cover */}
-      <div className="bg-yellow-300 w-full h-1/2 rounded-t-xl">
-        프로젝트 이미지
+      <div className="   bg-yellow-300 w-full h-1/2 rounded-t-xl">
+        <img
+          className="w-full h-full object-cover  rounded-t-xl"
+          src={`${process.env.PUBLIC_URL}/${image}`}
+          alt=""
+        />
       </div>
-      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-      <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+      <div className="text-xl font-bold mt-2 mx-2">{title}</div>
+      <div className="mt-2 mx-2">{desc}</div>
     </li>
   );
 };
