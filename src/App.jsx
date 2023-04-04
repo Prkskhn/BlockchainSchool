51a72c968a9011aa1bdc656ceadd6c028b823d77
Prkskhn <tjrgns7452@gmail.com>
2023-04-04 19:07:41 +0900

    web

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
 function App() {
-  return <div className="App">11</div>;
+  return (
+    <div className="  bg-red-100 min-w-screen min-h-screen flex flex-col justify-center items-center   ">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+function Main() {
+  return (
+    <div className="bg-purple-100 min-w-full flex justify-center items-center grow">
+      <div></div>
+      <div>main</div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Main;
