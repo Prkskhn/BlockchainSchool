e706c07da0d7981a55a549c19ce24a3c5b319553
Prkskhn <tjrgns7452@gmail.com>
2023-04-04 14:33:19 +0900

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./components/Header";
+import Introduce from "./components/Introduce";
+import Portfolio from "./components/Portfolio";
+import ContactMe from "./components/ContactMe";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="bg-red-100 min-h-screen">
+      <Header />
+      <main>
+        <Introduce />
+        <Portfolio />
+        <ContactMe />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Introduce.jsx
@@ -0,0 +1,9 @@
+const Introduce = () => {
+  return (
+    <div className="bg-green-300 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+      내 소개 내용
+    </div>
+  );
+};
+
+export default Introduce;
