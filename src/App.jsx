8adcfe9a4e6628ae66ad56c2ee422efb428877fd
Prkskhn <tjrgns7452@gmail.com>
2023-04-04 11:50:56 +0900

    useState useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
+
 function App() {
-  return <div className="App">11</div>;
+  const [toggle, setToggle] = useState(false);
+  const onClickToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      {toggle && <Counter />}
+      <button
+        onClick={onClickToggle}
+        className="border-2 border-red-300 px-3 py-1 m-4 text-red-500 font-bold bg-white rounded-md "
+      >
+        버튼
+      </button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const onClickAdd = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    console.log("CounterComponent가 생성된 상태입니다!");
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-center text-4xl">{count}</div>
+      <button
+        onClick={onClickAdd}
+        className="bg-red-500 px-4 py-2 rounded-2xl text-white font-bold mt-4"
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
