869f628d0df099418c2f6cfe315a732c1c87b9bd
Prkskhn <tjrgns7452@gmail.com>
2023-04-05 15:14:15 +0900

    props hell

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+import Child from "./components/Child";
+import ChildFriend from "./components/ChildFriend";
+
 function App() {
-  return <div className="App">11</div>;
+  const [gift, setGift] = useState(10000);
+
+  return (
+    <div className="bg-red-100">
+      <Child gift={gift} setGift={setGift} />
+      <ChildFriend gift={gift} setGift={setGift} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/GrandChild.jsx
@@ -0,0 +1,11 @@
+import SuperGrandChild from "./SuperGrandChild";
+
+const GrandChild = ({ gift, setGift }) => {
+  return (
+    <div>
+      <div className="w-40 h-40 bg-blue-300">GrandChild: {gift}</div>
+      <SuperGrandChild gift={gift} setGift={setGift} />
+    </div>
+  );
+};
+export default GrandChild;
