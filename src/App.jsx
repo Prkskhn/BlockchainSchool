f64d12708d7d32644ca3edd78fd2fdf59696d765
Prkskhn <tjrgns7452@gmail.com>
2023-04-05 09:55:47 +0900

    functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import ClassComp2 from "./components/ClassComp2";
 import ClassComp from "./components/ClassComponent";
+import Counter from "./components/Counter";
 
 function App() {
   return (
-    <div className="bg-red-300">
+    <div className="bg-red-300 min-w-full min-h-screen">
       <ClassComp />
+      {/* <ClassComp2 /> */}
+      <Counter />
     </div>
   );
 }
--- src/components/ClassComponent.jsx
+++ src/components/ClassComponent.jsx
@@ -13,9 +13,14 @@ class ClassComp extends Component {
 
   render() {
     return (
-      <div>
-        {this.state.count}
-        <button onClick={() => this.setCount(this.state.count + 1)}>+</button>
+      <div className="bg-blue-100 w-1/2 min-h-screen flex flex-col justify-center items-center text-2xl ">
+        <div>{this.state.count}</div>
+        <button
+          className="bg-purple-300 px-4 py-2 rounded-md m-2 flex justify-center items-center"
+          onClick={() => this.setCount(this.state.count + 1)}
+        >
+          +
+        </button>
       </div>
     );
   }
