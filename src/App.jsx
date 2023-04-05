2e75c875721f36d3d44033f61972c9d03623b9cf
Prkskhn <tjrgns7452@gmail.com>
2023-04-05 10:51:49 +0900

    set useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import GameBoard from "./components/GameBoard";
+import GameResult from "./components/GameResult";
+
 function App() {
-  return <div className="App">11</div>;
+  return (
+    <div className="bg-red-200 flex flex-col justify-center items-center min-h-screen">
+      <GameResult />
+      <GameBoard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/GameResult.jsx
@@ -0,0 +1,4 @@
+const GameResult = () => {
+  return <div className="bg-yellow-100 w-full grow">게임결과</div>;
+};
+export default GameResult;
