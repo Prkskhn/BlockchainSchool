ade615c5a723f2e1bc900d5ff3edaf769220142a
Prkskhn <tjrgns7452@gmail.com>
2023-04-06 15:58:29 +0900

    worldcup finish

--- src/pages/worldcup.jsx
+++ src/pages/worldcup.jsx
@@ -1,20 +1,59 @@
 import { useEffect, useState } from "react";
 import animalData from "../animalData.json";
+import AnimalCard from "../components/AnimalCard";
+import WinAnimalCard from "../components/WinAnimalCard";
 
 const Worldcup = () => {
   const [shuffleAnimal, setShuffleAnimal] = useState();
+  const [choice, setChoice] = useState(0);
+  const [nextRound, setNextRound] = useState([]);
+  const [end, setEnd] = useState(16);
 
-  console.log(animalData);
+  const onClickChoice = (v) => () => {
+    setChoice(choice + 2);
+    setNextRound([...nextRound, v]);
+  };
+
   useEffect(() => {
     let shuffleAnimalData = animalData.sort(() => {
-      return -1;
+      return Math.random() - 0.5;
     });
 
-    console.log(shuffleAnimalData);
+    setShuffleAnimal(shuffleAnimalData);
   }, []);
+  useEffect(() => console.log(nextRound), [nextRound]);
+
+  useEffect(() => {
+    if (choice === end) {
+      console.log("한 라운드 종료");
+      setShuffleAnimal(nextRound);
+      setNextRound([]);
+      setEnd(end / 2);
+      setChoice(0);
+    }
+  }, [choice]);
+
   return (
-    <div className="bg-yellow-300 min-h-screen flex flex-col justify-center items-center">
-      Worldcup
+    <div className="bg-yellow-300 min-h-screen flex  justify-center items-center">
+      {/* {shuffleAnimal ? <AnimalCard animal={shuffleAnimal[8]} /> : "값이없음"} */}
+      {shuffleAnimal &&
+        (end === 1 ? (
+          <WinAnimalCard animal={shuffleAnimal[choice]} />
+        ) : (
+          <>
+            <AnimalCard
+              animal={shuffleAnimal[choice]}
+              choice={choice}
+              onClickChoice={onClickChoice}
+            />
+            <div className="text-2xl mx-8 font-bold">VS</div>
+            <AnimalCard
+              animal={shuffleAnimal[choice + 1]}
+              choice={choice + 1}
+              onClickChoice={onClickChoice}
+            />
+          </>
+        ))}
     </div>
   );
 };
