bca5b4aaa8cf78c80f789e1a27ce8f080a74be9f
Prkskhn <tjrgns7452@gmail.com>
2023-04-11 15:00:40 +0900

    practice filter

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const todoRouter = require("./routes/todo");
 const app = express();
 const port = 3010;
+
+app.use(express.json());
+app.use("/todo", todoRouter);
+
 app.get("/", (req, res) => {
   res.send("Hello,Express");
 });
---
+++ routes/todo.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+let todoData = require("../todoData.json");
+
+router.get("/", (req, res) => {
+  console.log(todoData);
+  res.json(todoData);
+});
+router.post("/", (req, res) => {
+  const { title, desc } = req.body;
+
+  todoData.push({ title, desc, isDone: false });
+  console.log(todoData);
+  res.send(todoData);
+});
+
+router.put("/done/:id", (req, res) => {
+  const { id } = req.params;
+
+  todoData[parseInt(id)] = {
+    title: todoData[parseInt(id)].title,
+    desc: todoData[parseInt(id)].desc,
+    isDone: !todoData[parseInt(id)].isDone,
+  };
+
+  console.log(todoData);
+
+  res.send(todoData);
+});
+
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;
