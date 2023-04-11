79c909368212a749159a51acd68149c5a4e7d666
Prkskhn <tjrgns7452@gmail.com>
2023-04-11 19:11:30 +0900

    put delete

--- routes/todo.js
+++ routes/todo.js
@@ -14,15 +14,26 @@ router.post("/", (req, res) => {
   console.log(todoData);
 });
 
-// router.put("/:id", (req, res) => {
-//   const { id } = req.params;
-//   res.send(todoData[parseInt(id)]);
-//   console.log(todoData);
-// });
-// router.delete("/:id", (req, res) => {
-//   const { id } = req.params;
-//   res.send(todoData[parseInt(id)]);
-//   console.log(todoData);
-// });
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, desc } = req.body;
+
+  todoData[parseInt(id)] = {
+    title,
+    desc,
+  };
+
+  res.send(todoData);
+  console.log(todoData);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  todoData.filter((v, i) => {
+    return parseInt(id) !== i;
+  });
+  res.send(todoData);
+  console.log(todoData);
+});
 
 module.exports = router;
