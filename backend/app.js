27422d7bc2527674bdd42ab22c2d2ff1474cf571
Prkskhn <tjrgns7452@gmail.com>
2023-04-13 11:50:20 +0900

    re finish

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,13 @@ const app = express();
 
 const port = 3030;
 
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
+
 app.use(cors());
 app.use(express.json());
 app.use("/todo", todoRouter);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react";
+import ToDoCard from "./components/ToDoCard";
+import axios from "axios";
+import CreateToDo from "./components/CreateToDo";
+
 function App() {
+  const [toDoList, setToDoList] = useState();
+
+  const getToDoList = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/todo`
+      );
+
+      if (response.status !== 200) {
+        alert("에러 발생!");
+        return;
+      }
+
+      setToDoList(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getToDoList();
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col justify-start items-center pt-16">
       <h1 className="text-4xl font-bold">AWESOME TO DO LIST 😎</h1>
@@ -11,30 +39,22 @@ function App() {
           나무 베는데 한 시간이 주어진다면, 도끼를 가는데 45분을 쓰겠다,
           에비브러햄 링컨
         </div>
-        <form className="flex mt-2">
-          <input
-            className="grow border-2 border-pink-200 rounded-lg focus:outline-pink-400 px-2 py-1 text-lg"
-            type="text"
-          />
-          <input
-            className="ml-4 px-2 py-1 bg-pink-400 rounded-lg text-gray-50"
-            type="submit"
-            value="새 투두 생성"
-          />
-        </form>
+        <CreateToDo getToDoList={getToDoList} />
       </div>
       <div className="mt-16 flex flex-col w-1/2">
-        <div className="flex my-4">
-          <div className="border-4 border-pink-400 w-8 h-8 rounded-xl"></div>
-          <div className="text-2xl ml-4">🧹 청소하기</div>
-        </div>
-        <div className="flex my-4">
-          <div className="relative">
-            <div className="border-4 border-pink-400 w-8 h-8 rounded-xl bg-pink-400 p-2"></div>
-            <div className="absolute border-4 border-white top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-8 h-8 scale-75 rounded-xl bg-pink-400 p-2"></div>
-          </div>
-          <div className="text-2xl ml-4">👕 빨래하기</div>
-        </div>
+        {toDoList
+          ? toDoList.map((v, i) => {
+              return (
+                <ToDoCard
+                  title={v.title}
+                  key={i}
+                  index={i}
+                  getToDoList={getToDoList}
+                  isDone={v.isDone}
+                />
+              );
+            })
+          : "로딩중입니다."}
       </div>
     </div>
   );
