8128e34cfae39c0992d9dc1e5f209c5a981e45ef
Prkskhn <tjrgns7452@gmail.com>
2023-04-20 14:12:53 +0900

    get account

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  const [account, setAccount] = useState("");
+
+  const onClickAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      {account ? (
+        account
+      ) : (
+        <button className="btn-style" onClick={onClickAccount}>
+          <img
+            className="w-12"
+            src={`${process.env.PUBLIC_URL}/images/metamask.png`}
+          />
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default App;
