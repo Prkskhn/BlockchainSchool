9d660f6995770becbe4eb13bc2be2d9b9304ae1c
Prkskhn <tjrgns7452@gmail.com>
2023-05-08 19:51:24 +0900

    plus

--- 20230508_basic_2.sol
+++ 20230508_basic_2.sol
@@ -35,16 +35,20 @@ contract Address{
         return bytes20(a);
     }
 
-    // uint b;
+    // uint b; // 상태변수
 
-    // function changB()public{
-    //     b=b+5;
+    // // 상태변수 변화 -> gas비 소비, 잔액 변화
+    // function changeB() public {
+    //     b = b+5;
     // }
-    // function add(uint _a,uint _b)public pure returns(uint){
+
+    // // pure 함수 -> 잔액 변화 x
+    // function add(uint _a, uint _b) public pure returns(uint) {
     //     return _a+_b;
     // }
-    //멍청하게 가스 낭비하는 짓거리
-    // function add2(uint _a,uint _b)public  returns(uint){
+
+    // // pure와 같은 역할이지만 일반 함수 -> gas비 소비, 잔액 변화
+    // function add2(uint _a, uint _b) public returns(uint) {
     //     return _a+_b;
     // }
 }
